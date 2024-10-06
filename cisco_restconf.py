80bd17a8aa7817db82f703b49af18415ca1bea12
itsec-lib <56153309+itsec-lib@users.noreply.github.com>
2024-10-06 19:47:01 +1100

    Create cisco_restconf.py

---
+++ cisco_restconf.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_interface_info(to_terminal=False):
+    """ 
+    Returns interface information in XML format 
+    If to_terminal=True prints data instead of returning
+    """
+    api_root = 'https://10.254.0.1:443/restconf'
+    dn = '/data/Cisco-IOS-XE-native:native/interface/'
+    response = requests.get(api_root + dn, 
+    auth=('cisco', 'cisco'), 
+    verify=False
+    )
+    if to_terminal == True:
+        print(response.content.decode('utf-8'))  
+    else:
+        return response.content
+    
+xml_data = get_interface_info()
+soup = BeautifulSoup(xml_data, 'lxml')
+#print(soup.prettify())
+for intf in soup.find_all('gigabitethernet'):
+    print('-' * 25)
+    ip = intf.find('ip')
+    if ip:
+        print('GigabitEthernet', intf.find('name').string)  
+        print(intf.ip.primary.address)
+
+
